94e96641b1c03a5b1a8b8af3f5eae04a0f21d1b2
Shreeya Shah <shreeya.chauhan61@gmail.com>
2025-05-07 15:34:20 -0400

    folder structure addded

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Shreeya Shah',
+    author_email= 'shreeya.chauhan61@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
